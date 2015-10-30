ccfa40dd87a4993b683b29b98aea3122818a82f5
yyrdl <yyrdl1215@163.com>
2015-10-30 12:19:43 +0800

    f

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
 /**
  * Created by zj on 2015/10/25.
  */
+
+$(function(){
+    $(".book_item").on("mouseover",function(){
+        var hot=$(this).find(".hot_info");
+        if(hot.data('isMoved')){
+        }else{
+            hot.data("isMoved",true);
+            setTimeout(function(){
+                hot.css({marginLeft:'0%'});
+            },0);
+        }
+    });
+    $(".book_item").on("mouseout",function(){
+        var hot=$(this).find(".hot_info");
+        hot.data("isMoved",false);
+        setTimeout(function(){
+            var obj={marginLeft:"-100%"};
+            hot.css(obj);
+        },0);
+    });
+    $(".sidebar>ul>li").on("click",function(){
+        var b=$(this).find("a").find("b");
+        ($(b).text()=='[+]'&&($(b).text('[-]')))||($(b).text('[+]'));
+    })
+});
