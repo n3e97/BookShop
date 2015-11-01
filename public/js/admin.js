f2646cecc0be98d03a1a96759d542ba86473dfa2
yyrdl <yyrdl1215@163.com>
2015-11-01 16:54:08 +0800

    完成了后台管理界面的html模板

--- public/js/admin.js
+++ public/js/admin.js
@@ -120,9 +120,85 @@ $(function(){
     }
 
     $(function(){
-      bind_search_event();
+     // bind_search_event();
     });
     w.my_nextPage=nextPage;
     w.my_prevPage=prevPage;
     w.my_toPage=toPage;
 })(window);
+
+$(function(){
+   $(".edit_border").on("mousedown",function(e){
+       var edit=$(this).parent(),
+           preview=$(this).parent().parent().find(".preview_area");
+       var x= e.clientX;
+       $(window).on("mouseup",function(){
+           $(window).off("mousemove");
+           $(window).off("mouseup");
+       });
+       $(window).on("mousemove",function(ee){
+           var offset=ee.clientX-x;
+           x=ee.clientX;
+           var w_e=parseFloat($(edit).css('width')),
+               w_p=parseFloat($(preview).css("width"));
+           w_e+=offset;
+           w_p-=offset;
+           $(edit).css({width:w_e+"px"});
+           $(preview).css({width:w_p+"px"});
+       });
+   });
+   $(".edit_area>textarea").on("input propertychange",function(){
+          var self=this;
+          $(self).parent().parent().find(".preview_area").html(markdown.toHTML($(self).get(0).value));
+   });
+    //È«ÆÁ
+    $(".book_item_full_screen").on("click",function(){
+          var self=this;
+          var width=window.screen.availWidth,
+              height=window.screen.availHeight;
+          var dt=$(self).data("isfull");
+          dt==undefined&&($(self).data("isfull",false));
+          dt=$(self).data("isfull");
+          if(!dt){
+              var or_width=$(self).parent().parent().parent().css("width");
+              $(self).data("or_w",or_width);
+              $(self).parent().parent().parent().css({position:"absolute",zIndex:10000,backgroundColor:"white",overflow:"auto"});
+              $(self).parent().parent().parent().velocity({width:width+"px",height:height+"px",left:"0px",top:"0px"},100,"ease-in-out",function(){
+                  $(self).find("i").removeClass("fa-expand").addClass("fa-compress");
+                  $(self).data("isfull",true);
+              });
+          }else{
+              var the_w=$(self).data("or_w");
+              $(self).parent().parent().parent().velocity("reverse",function(){
+                  $(self).parent().parent().parent().css({position:"inherit",zIndex:0,width:the_w});
+                  $(self).data("isfull",false).find("i").removeClass("fa-compress").addClass("fa-expand");
+              });
+          }
+    });
+    //ÐÞ¸Ä
+    var is_disabled=function(parent,b){
+        var obj={
+            book_name:$(parent).find(".book_item_name").attr("disabled",b),
+            book_author:$(parent).find(".book_item_author").attr("disabled",b),
+            book_banben:$(parent).find(".book_item_banben").attr("disabled",b),
+            book_publisher:$(parent).find(".book_item_publisher").attr("disabled",b),
+            book_price:$(parent).find(".book_item_price").attr("disabled",b),
+            book_kucun:$(parent).find(".book_item_kucun").attr("disabled",b),
+            book_tag:$(parent).find(".book_item_tag").attr("disabled",b),
+            book_info:$(parent).find(".book_info")
+        };
+        obj.book_info.find("textarea").attr("disabled",b);
+        return obj;
+    }
+    $(".book_item_edit").on("click",function(){
+        var parent=$(this).parent().parent().parent();
+        is_disabled(parent,false);
+    });
+    $(".book_item_save").on("click",function(){
+        var parent=$(this).parent().parent().parent();
+        var obj=is_disabled(parent,true);
+        $(this).parent().find(".book_item_refresh").css({visibility:"visible"});
+        //to do:....
+    });
+
+});
